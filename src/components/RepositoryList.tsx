66dc979ab8f8f214617e37f14bc77efc956f1a2e
KleytonLeite <musicpteclados@gmail.com>
2021-03-24 14:52:40 -0300

    faet: transformando componentes jsx em tsx

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,12 +3,16 @@ import { RepositoryItem } from "./repositoryItem";
 
 import "../styles/repositories.scss";
 
-// https://api.github.com/orgs/rocketseat/repos
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
 
 
  export function RepositoryList() {
 
-    const [ repositories, setRepositories] = useState([]);
+    const [ repositories, setRepositories ] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
